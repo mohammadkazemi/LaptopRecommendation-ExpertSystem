9b4b0b667c71db24a91985f99bdc4e6f185b5ef7
mohammadkazemi <mohammadkazemiwork@gmail.com>
2021-01-08 19:47:18 +0330

    adding .venv adding .gitignore

---
+++ inference_engine.py
@@ -0,0 +1,287 @@
+import tkinter as tk
+from enum import Enum
+import csv
+from knowledge_base import Laptop, is_studio, isclerk, isdev, ishome, isgaming, limit_price_rule_checker
+
+
+def laptop_maker(id, brand, model, ram, hd_type, hd_size, screen_size, price, processor_brand, processor_model,
+                 clock_speed, graphic_card_brand,
+                 graphic_card_size, os, weight, comments):
+    laptop = Laptop(laptop=id,
+                    brand=brand,
+                    model=model,
+                    ram=ram,
+                    hd_type=hd_type,
+                    hd_size=hd_size,
+                    screen_size=screen_size,
+                    price=price,
+                    processor_brand=processor_brand,
+                    processor_model=processor_model,
+                    clock_speed=clock_speed,
+                    gc_brand=graphic_card_brand,
+                    gc_size=graphic_card_size,
+                    os=os,
+                    weight=weight,
+                    comments=comments)
+
+    if clock_speed:
+        laptop.clock_speed = clock_speed
+    else:
+        laptop.clock_speed = 0
+    if graphic_card_brand:
+        laptop.gc_brand = graphic_card_brand
+    else:
+        laptop.gc_brand = ''
+
+    if graphic_card_size:
+        laptop.gc_size = graphic_card_size
+    else:
+        laptop.gc_size = 0
+
+    if weight:
+        laptop.weight = weight
+    else:
+        laptop.weight = 0
+    if comments:
+        laptop.comments = comments
+    else:
+        laptop.comments = ''
+
+    if os:
+        laptop.os = os
+    else:
+        laptop.os = ''
+    return laptop
+
+
+def database_builder():
+    # reading from database
+    csvfile = open("laptops.csv", newline='')
+    reader = csv.DictReader(csvfile)
+    i = 1
+    database = [Laptop]
+    for row in reader:
+        row['id'] = i
+        i = i + 1
+        row['comments'] = row['comments'].replace('\n', ' ')
+        database.append(laptop_maker(
+            id=row['id'],
+            brand=row['brand'],
+            model=row['model'],
+            ram=row['ram'],
+            hd_type=row['hd_type'],
+            hd_size=row['hd_size'],
+            screen_size=row['screen_size'],
+            price=row['price'],
+            processor_brand=row['processor_brand'],
+            processor_model=row['processor_model'],
+            clock_speed=row['clock_speed'],
+            graphic_card_brand=row['graphic_card_brand'],
+            graphic_card_size=row['graphic_card_size'],
+            os=row['os'],
+            weight=row['weight'],
+            comments=row['comments'],
+        ))
+    return database
+
+
+# start writing gui for program
+root = tk.Tk()
+root.title("laptop recommendation expert system")
+
+
+class States(Enum):
+    LAPTOP_TYPES_STATE = 1
+    UPPER_BOUND_PRICE_STATE = 2
+    OFFICE_TYPE_LAPTOP_STATE = 3
+    DEV_TYPE_LAPTOP_STATE = 4
+    SHOW_RESULT_LAPTOPS_STATE = 5
+
+
+class AllLabels(Enum):
+    LAPTOP_TYPES_LABEL = 'which type of laptop do you want?'
+    UPPER_BOUND_PRICE_LABEL = 'Input Upper bound on Price '
+    OFFICE_TYPE_LAPTOP_LABEL = 'what kind of office laptop do you want ?'
+    DEV_TYPE_LAPTOP_LABEL = 'what kind of development laptop do you want?'
+    SHOW_RESULT_LAPTOPS_LABEL = 'with your choices this laptops is available :'
+
+
+office_laptops = [('clerk', 108),
+                  ('development', 109),
+                  ('studio', 110)]
+
+dev_laptops = [('mobile development', 104),
+               ('web and scripting', 105),
+               ('game development', 106),
+               ('data science', 107)]
+
+laptop_types = [("gaming", 101),
+                ("home", 102),
+                ("office", 103)
+                ]
+
+user_upper_bound_value = 0
+laptop_choice = 0
+office_laptop_choice = 0
+development_laptop_choice = 0
+
+
+def find_best_laps() -> [Laptop]:
+    database = database_builder()
+    final_database = [Laptop]
+    show_to_user = [Laptop]
+    del database[0]
+
+    if laptop_choice == 101:
+        # gaming
+        for lap in database:
+            if isgaming(lap):
+                final_database.append(lap)
+    if laptop_choice == 102:
+        # home
+        for lap in database:
+            if ishome(lap):
+                final_database.append(lap)
+    if laptop_choice == 103:
+        # office
+        if office_laptop_choice == 108:
+            # clerk
+            for lap in database:
+                if isclerk(lap):
+                    final_database.append(lap)
+        elif office_laptop_choice == 109:
+            # development
+            for lap in database:
+                if isdev(lap):
+                    final_database.append(lap)
+
+            if development_laptop_choice == 104:
+                # mobile development
+                pass
+            elif development_laptop_choice == 105:
+                # web and scripting
+                pass
+            elif development_laptop_choice == 106:
+                #  game development
+                pass
+            else:
+                # data science
+                pass
+        else:
+            # office -> studio
+            for lap in database:
+                if is_studio(lap):
+                    final_database.append(lap)
+
+    show_to_user = limit_price_rule_checker(final_database, user_upper_bound_value)
+
+
+
+def render_screen(next_state: States):
+    root.destroy()
+    root.__init__()
+    v = tk.IntVar()
+    v.set(1)  # initializing the choice, i.e. Python
+
+    def radio_btn_builder(show_choice, v, iterable_object):
+        for iter_obj, val in iterable_object:
+            tk.Radiobutton(root,
+                           text=iter_obj,
+                           padx=20,
+                           variable=v,
+                           command=show_choice,
+                           value=val).pack(anchor=tk.W)
+
+    def get_upper_bound_value(value):
+        global user_upper_bound_value
+        user_upper_bound_value = value
+        return render_screen(States.SHOW_RESULT_LAPTOPS_STATE)
+
+    def show_choice():
+        global laptop_choice
+        global office_laptop_choice
+        global development_laptop_choice
+
+        print(v.get())
+        if v.get() == 101:
+            # 'gaming',             101
+            laptop_choice = 101
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 102:
+            # 'home',               102
+            laptop_choice = 102
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 103:
+            # 'office',             103
+            laptop_choice = 103
+            return render_screen(States.OFFICE_TYPE_LAPTOP_STATE)
+        if v.get() == 104:
+            # 'mobile development', 104
+            development_laptop_choice = 104
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 105:
+            # 'web and scripting',  105
+            development_laptop_choice = 105
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 106:
+            # 'game development',   106
+            development_laptop_choice = 106
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 107:
+            # 'data science',       107
+            development_laptop_choice = 107
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 108:
+            # 'clerk',              108
+            office_laptop_choice = 108
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+        if v.get() == 109:
+            # 'development',        109
+            return render_screen(States.DEV_TYPE_LAPTOP_STATE)
+        if v.get() == 110:
+            # 'studio',             110
+            return render_screen(States.UPPER_BOUND_PRICE_STATE)
+
+    if next_state == States.LAPTOP_TYPES_STATE:
+        return (tk.Label(root, text=AllLabels.LAPTOP_TYPES_LABEL.value).pack(),
+                radio_btn_builder(show_choice, v, laptop_types))
+
+    if next_state == States.UPPER_BOUND_PRICE_STATE:
+        l1: tk.Label = tk.Label(
+            root, text=AllLabels.UPPER_BOUND_PRICE_LABEL.value)
+        ent: tk.Entry = tk.Entry(root)
+        btn: tk.Button = tk.Button(root, text='Submit', fg='red',
+                                   command=lambda: get_upper_bound_value(ent.get()))
+
+        return l1.pack(), ent.pack(), btn.pack()
+
+    if next_state == States.OFFICE_TYPE_LAPTOP_STATE:
+        return (tk.Label(root, text=AllLabels.OFFICE_TYPE_LAPTOP_LABEL.value).pack(),
+                radio_btn_builder(show_choice, v, office_laptops))
+
+    if next_state == States.DEV_TYPE_LAPTOP_STATE:
+        return (tk.Label(root, text=AllLabels.DEV_TYPE_LAPTOP_LABEL.value).pack(),
+                radio_btn_builder(show_choice, v, dev_laptops))
+
+    if next_state == States.SHOW_RESULT_LAPTOPS_STATE:
+        best_laps_str: [Laptop] = find_best_laps()
+        lbl1 = tk.Label(root, text=AllLabels.SHOW_RESULT_LAPTOPS_LABEL.value).pack()
+        lbl2 = tk.Label(root, text='laptop 1'
+                                   'laptop 2'
+                                   'laptop 3'
+                                   'laptop 4').pack()
+        btn = tk.Button(root,
+                        text="QUIT",
+                        fg="red",
+                        command=quit).pack()
+
+        return (lbl1, lbl2, btn)
+
+
+render_screen(States.LAPTOP_TYPES_STATE)
+exit_button = tk.Button(root,
+                        text="QUIT",
+                        fg="red",
+                        command=quit).pack()
+
+root.mainloop()
---
+++ knowledge_base.py
@@ -0,0 +1,319 @@
+from enum import Enum
+
+
+class Laptop:
+    ''' laptop object model =>
+    id,brand,model,ram,hd_type,hd_size,screen_size,price,processor_brand,processor_model,clock_speed,graphic_card_brand,graphic_card_size,os,weight,comments
+    exmaple =>
+    35,HP,Omen W 250TX,16,ssd,256,17.6,169990,intel,i7,3.6,nvidia,8,windows,2.7,"The Best Gaming is on GeForce
+    Discover desktop-class gaming on a notebook with the next-generation GeForce GTX 1050M/1060M/1070M.** Plus, get improved battery life you need to game longer, unplugged."
+    '''
+
+    def __init__(self
+                 , laptop
+                 , brand
+                 , model
+                 , ram
+                 , hd_type
+                 , hd_size
+                 , screen_size
+                 , price
+                 , processor_brand
+                 , processor_model
+                 , clock_speed=None
+                 , gc_brand=None
+                 , gc_size=None
+                 , os=None
+                 , weight=None,
+                 comments=None
+                 ):
+        self.laptop = laptop
+        self.brand = brand
+        self.model = model
+        self.ram = ram
+        self.hd_type = hd_type
+        self.hd_size = hd_size
+        self.screen_size = screen_size
+        self.price = price
+        self.processor_brand = processor_brand
+        self.processor_model = processor_model
+        self.clock_speed = clock_speed
+        self.gc_brand = gc_brand
+        self.gc_size = gc_size
+        self.os = os
+        self.weight = weight
+        self.comments = comments
+
+    def __cmp__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __ne__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
+
+    def __str__(self):
+        return f' #laptop number: {self.laptop} ' \
+               f' #brand: {self.brand} ' \
+               f' #model: {self.model} ' \
+               f' #ram: {self.ram} -' \
+               f' #hd_type: {self.hd_type} -' \
+               f' #hd_size: {self.hd_size} -' \
+               f' #screen_size: {self.screen_size} ' \
+               f' #price: {self.price} ' \
+               f' #processor_brand: {self.processor_brand} ' \
+               f' #processor_model: {self.processor_model} ' \
+               f' #clock_speed: {self.clock_speed} ' \
+               f' #gc_brand: {self.gc_brand}' \
+               f' #gc_size: {self.gc_size} ' \
+               f' #os: {self.os} ' \
+               f' #weight: {self.weight} ' \
+               f' #comments: {self.comments}'
+
+
+class IntelModels(Enum):
+    i3 = 'i3'
+    i5 = 'i5'
+    i7 = 'i7'
+    i9 = 'i9'
+
+
+# def rule_compare_storage_type(laptop_a:Laptop, laptop_b:Laptop):
+#     '''ssd is better than hdd'''
+#     pass
+
+
+# def compare_gc_size(P, Q):
+#     '''more memory is better than less memory'''
+#     gc_size(P, R),
+#     gc_size(Q, S),
+#     X = R, Y = S,
+#     X >= Y.
+
+
+# def compare_weight(P, Q):
+#     '''less laptop weight is better than more laptop weight'''
+#     weight(P, R),
+#     weight(Q, S),
+#     X = R, Y = S,
+#     X >= Y.
+
+
+# def compare_price(P, Q):
+#     '''less laptop price is better tham more laptop price'''
+#     price(P, R),
+#     price(Q, S),
+#     X = R, Y = S,
+#     X >= Y.
+
+
+# def compare_screen_size(P, Q):
+#     '''more laptop screen is better than less laptop screen'''
+#     screen_size(P, R),
+#     screen_size(Q, S),
+#     X = R, Y = S,
+#     X >= Y.
+
+
+# def compare_processors_brand(P, Q):
+#     ''' intel is better than amd '''
+#     A = 1, B = 2, B > A,
+#     processor_brand(P, R),
+#     processor_brand(Q, S),
+#     X = R, Y = S,
+#     ((X = 'amd', Y = 'amd')
+#      (X='intel', Y='amd')
+#      (X='intel', Y='intel')).
+
+
+# def compare_graphics(P, Q):
+#     ''' nvidia is better than amd'''
+#     ''' nvidia is better than intel'''
+#     ''' amd is better than intel'''
+#     A = 1, B = 2, B > A,
+#     gc_brand(P, R),
+#     gc_brand(Q, S),
+#     X = R, Y = S,
+#     ((X = 'nvidia', Y = 'amd')
+#      (X='nvidia', Y='intel')
+#      (X='intel', Y='intel')
+#      (X='nvidia', Y='nvidia')
+#      (X='amd', Y='amd')
+#      (X='amd', Y='intel')).
+
+
+# def compare_processors(P, Q):
+#     ''' i9 is better than all '''
+#     ''' i7 is better than i5, i3'''
+#     ''' i5 is better than i3'''
+#     A = 1, B = 2, B > A,
+#     processor_model(P, R),
+#     processor_model(Q, S),
+#     X = R, Y = S,
+#     ((X = 'i5', Y = 'i3')
+#      (X='i7', Y='i3')
+#      (X='i5', Y='i5')
+#      (X='i3', Y='i3')
+#      (X='i7', Y='i7')
+#      (X='i7', Y='i5'))
+
+
+# def compare_laptop(X, Y):
+#     compare_price(X, Y)
+#     compare_processors_brand(X, Y) # its not necessory
+#     compare_processors(X, Y)
+#     compare_gc_size(X, Y)
+#     compare_screen_size(X, Y)
+
+
+def limit_price_rule_checker(laptops: [Laptop], upper_bound):
+    tmp = [Laptop]
+    for lap in laptops:
+        if int(lap.price) < int(upper_bound):
+            tmp.append(lap)
+    return tmp
+    # return tmp
+    # return list(map(lambda x: int(x.price) < int(upper_bound), laptops))
+
+
+def screen_size_rule_checker(laptop: Laptop, needed_screen_size, gt: bool, lt: bool):
+    if gt:
+        if float(laptop.screen_size) >= needed_screen_size:
+            return True
+        else:
+            return False
+    elif lt:
+        if float(laptop.screen_size) <= needed_screen_size:
+            return True
+        else:
+            return False
+
+
+def processor_brand_rule_checker(laptop: Laptop, needed_model: [IntelModels] = None):
+    if str(laptop.processor_brand).lower() == "intel":
+        if str(laptop.processor_model).lower() in needed_model:
+            return True
+        else:
+            return False
+    elif str(laptop.processor_brand).lower() == "amd":
+        return True
+    else:
+        return False
+
+
+def gc_size_rule_checker(laptop: Laptop, needed_memory_size, gt: bool, lt: bool):
+    if gt:
+        if int(laptop.gc_size) >= needed_memory_size:
+            return True
+        else:
+            return False
+    elif lt:
+        if int(laptop.gc_size) <= needed_memory_size:
+            return True
+        else:
+            return False
+
+
+def os_rule_checker(laptop: Laptop, os: [str]):
+    if laptop.os in os:
+        return True
+    return False
+
+
+def ram_rule_checker(laptop: Laptop, needed_ram, gt: bool, lt: bool):
+    if gt:
+        if int(laptop.ram) >= needed_ram:
+            return True
+        else:
+            return False
+    elif lt:
+        if int(laptop.ram) <= needed_ram:
+            return True
+        else:
+            return False
+
+
+def rule_hd_size(laptop: Laptop, needed_storage_size, gt: bool, lt: bool):
+    if gt:
+        if int(laptop.hd_size) >= needed_storage_size:
+            return True
+        else:
+            return False
+    elif lt:
+        if int(laptop.hd_size) <= needed_storage_size:
+            return True
+        else:
+            return False
+
+
+def rule_clock_speed(laptop: Laptop, needed_clock_speed, gt: bool, lt: bool):
+    if gt:
+        if float(laptop.clock_speed) >= needed_clock_speed:
+            return True
+        else:
+            return False
+    elif lt:
+        if float(laptop.clock_speed) <= needed_clock_speed:
+            return True
+        else:
+            return False
+
+
+def isgaming(laptop: Laptop):
+    tmp = (
+            ram_rule_checker(laptop=laptop, needed_ram=8, gt=True, lt=False) and
+            rule_hd_size(laptop=laptop, needed_storage_size=256, gt=True, lt=False) and
+            screen_size_rule_checker(laptop=laptop, needed_screen_size=15, gt=True, lt=False) and
+            gc_size_rule_checker(laptop=laptop, needed_memory_size=2, gt=True, lt=False) and
+            os_rule_checker(laptop=laptop, os='windows') and
+            processor_brand_rule_checker(laptop=laptop, needed_model=[IntelModels.i5, IntelModels.i7, IntelModels.i9])
+    )
+    return tmp
+
+
+def isdev(laptop: Laptop):
+    return (ram_rule_checker(laptop=laptop, needed_ram=8, gt=True, lt=False) and
+            rule_hd_size(laptop=laptop, needed_storage_size=256, gt=True, lt=False) and
+            screen_size_rule_checker(laptop=laptop, needed_screen_size=14, gt=True, lt=False) and
+            rule_clock_speed(laptop=laptop, needed_clock_speed=2.3, gt=True, lt=False) and
+            gc_size_rule_checker(laptop=laptop, needed_memory_size=1, gt=True, lt=False) and
+            os_rule_checker(laptop, ['windows', 'linux', 'mac']) and
+            processor_brand_rule_checker(laptop=laptop, needed_model=[IntelModels.i5, IntelModels.i7, IntelModels.i9]))
+
+
+def isclerk(laptop: Laptop):
+    return (ram_rule_checker(laptop=laptop, needed_ram=2, gt=True, lt=False) and
+            rule_hd_size(laptop, needed_storage_size=128, gt=True, lt=False) and
+            screen_size_rule_checker(laptop, needed_screen_size=12, gt=True, lt=False) and
+            processor_brand_rule_checker(laptop, needed_model=[IntelModels.i3, IntelModels.i5, IntelModels.i7,
+                                                               IntelModels.i9]) and
+            os_rule_checker(laptop, 'windows'))
+
+
+def ishome(laptop: Laptop):
+    return (ram_rule_checker(laptop=laptop, needed_ram=2, gt=True, lt=False) and
+            rule_hd_size(laptop, needed_storage_size=128, gt=True, lt=False) and
+            screen_size_rule_checker(laptop, needed_screen_size=13, gt=True, lt=False) and
+            os_rule_checker(laptop, ['windows', 'mac'])
+            )
+
+
+def is_studio(laptop: Laptop):
+    return (ram_rule_checker(laptop=laptop, needed_ram=16, gt=True, lt=False) and
+            rule_hd_size(laptop, needed_storage_size=2014, gt=True, lt=False) and
+            screen_size_rule_checker(laptop, needed_screen_size=13, gt=True, lt=False) and
+            os_rule_checker(laptop, ['windows', 'mac'])
+            )
